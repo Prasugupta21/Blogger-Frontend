c4879474498a79aaf9d1e001312bcf361a1767c8
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-07-30 12:09:14 +0530

    modified login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
     }
     try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
+      const res = await fetch('/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
